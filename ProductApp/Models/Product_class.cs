b3b2d854329142a725c7a7600e1a426f599ba487
17Sampada <91624838+17Sampada@users.noreply.github.com>
2024-07-15 00:30:18 +0530

    Add files via upload

---
+++ ProductApp/Models/Product_class.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Models
+{
+    internal class Product_class
+    {
+
+        /*public int Id { get; set; } // auto properties
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public double DiscountPercent { get; set; }*/
+
+        private int _id; string _name; double _price; double _discountPercentage;
+
+        public int Id { get; set; }
+            //set { _id = value; } get { return _id; } }
+
+        public string Name {set {  _name = value; } get { return _name; } }
+
+        public double Price { set { _price = value; } get { return _price; } }
+
+        public double DiscountPercent
+        {
+            set
+            {
+                if (value > 0.4)
+                    _discountPercentage = 0.4;
+                else if (value < 0.2)
+                    _discountPercentage = 0.2;
+                else
+                    _discountPercentage = value;
+
+            }
+
+            get { return _discountPercentage; }
+        }
+
+        public Product_class() //default constructor----> parameterless 
+        {
+            Console.WriteLine("welcome");
+
+        }
+
+        public Product_class(int id, string name, double price, double discountPercentage):this() // this() calls the default constructor first and then the cureent constructor executes
+        {
+            
+            Id = id;
+            Name = name;
+            Price = price;
+            DiscountPercent = discountPercentage;
+        }
+
+        public double CalculateDiscount()
+        {
+            return _price - _price * DiscountPercent / 100;
+           
+
+        }
+
+
+    }
+}
---
+++ ProductApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp.Models;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //product 1
+            Product_class product1 = new Product_class(1,"book", 50,0.1);
+            /*product1.Id=1;
+            product1.Name="Book";
+            product1.Price=50;
+            product1.DiscountPercent=20;*/
+
+            //product 2
+            Product_class product2 = new Product_class(2,"bag", 5000,0.3);
+           /* product1.Id=2;
+            product1.Name="Bag";
+            product1.Price=5000;
+            product1.DiscountPercent=33;*/
+
+            PrintProductDetails(product1);
+            Console.WriteLine("-----------------------------------------");
+            PrintProductDetails( product2);
+
+            
+        }
+        
+        static void PrintProductDetails(Product_class product)
+        {
+            Console.WriteLine($" Product ID: {product.Id}\n" +
+                $"Product Name: {product.Name}\n" +
+                $"Product Price: {product.Price}\n" +
+                $"Product Discount: {product.DiscountPercent}\n" +
+                $"Product Cost after discount: {product.CalculateDiscount()}");
+                
+                     
+        }
+
+
+    }
+}
