3da4f9abd4b2aeee224410d1e60c4b14513fd7cc
17Sampada <91624838+17Sampada@users.noreply.github.com>
2024-07-15 00:37:02 +0530

    Add files via upload

---
+++ TypeCasting/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            float b = a;
+
+            Console.WriteLine(b);
+            Console.WriteLine("Datatype of b: " + b.GetType());
+
+            float x = 3.5f;
+            int y = (int)x;
+
+          
+            string s = "hello";
+            object obj = s;
+
+            object obj1 = "hello";
+            string s1=(string)obj;
+
+            int i = 10;
+            object obj2 = i;
+
+            Object obj3 = 10;
+            int g = (int)obj3;
+
+
+
+
+        }
+    }
+}
