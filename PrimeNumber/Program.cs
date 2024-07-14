62fff89e5b301454facfedc9f00f2199482617a0
17Sampada <91624838+17Sampada@users.noreply.github.com>
2024-07-15 01:01:54 +0530

    Add files via upload

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, a = 0;
+            Console.WriteLine("Number to be checked:");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("It is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+        }
+    }
+}
