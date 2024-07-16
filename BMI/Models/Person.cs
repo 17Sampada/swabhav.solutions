df51f5b61536fa3dec3c417e3ffc23fa2b4a253e
17Sampada <91624838+17Sampada@users.noreply.github.com>
2024-07-17 01:17:22 +0530

    Add files via upload

---
+++ BMI/Models/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI.Models
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public double Height { get; set; }
+
+        public double Weight { get; set; }
+
+        static int DEFAULT_HEIGHT = 5;
+        static int DEFAULT_WEIGHT = 50;
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Height = DEFAULT_HEIGHT;
+            Weight = DEFAULT_WEIGHT;
+
+        }
+
+
+        public Person(int id, string name, int age, double height, double weight) : this(id, name, age)
+        {
+            Height = height;
+            Weight = weight;
+
+        }
+
+        public double CalculateBmi()
+        {
+            return Weight / (Height * Height);
+        }
+
+        public string GetBodyType()
+        {
+            double bmi = CalculateBmi();
+
+            if (bmi < 18.5)
+            {
+                return "Underweight";
+            }
+            else if (bmi >= 18.5 && bmi < 24.9)
+            {
+                return "Normal weight";
+            }
+            else if (bmi >= 25 && bmi < 29.9)
+            {
+                return "Overweight";
+            }
+            else
+            {
+                return "Obesity";
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"Person ID: {Id}\n" +
+                $"Name: {Name}\n" +
+                $"Age: {Age}\n" +
+                $"Height:{Height}\n" +
+                $"Weight:{Weight}\n"+
+                $"BMI: {CalculateBmi()}\n"+
+                $"Body Type: {GetBodyType()}";
+        }
+    }
+}   
+
+
+
+
+
+
+
+
+
+
---
+++ BMI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMI.Models;
+
+namespace BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "Sampada", 22, 1.75, 50);
+            Person person2 = new Person(2, "Swamini", 21);
+
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+
+
+        }
+    }
+}
