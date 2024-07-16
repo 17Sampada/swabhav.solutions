7101935940c8c5ec9fbc6ecde603eb0fcd90abb2
17Sampada <91624838+17Sampada@users.noreply.github.com>
2024-07-16 12:29:01 +0530

    Add files via upload

---
+++ PlayerApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using PlayerApp.models;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = new Player[]
+            {
+                new Player(101,"Sampada", 14),
+                new Player(102,"Swamini",15),
+                new Player(103,"Swati",16),
+                new Player(104,"Aagya", 17),
+                new Player(105,"Sam")
+
+            };
+
+            Player  eldestPlayer=Player.WhoIsElder(players);
+            Console.WriteLine($"Eldest Player's details is:");
+            Console.WriteLine(Player.WhoIsElder(players));
+
+
+        }
+
+
+    }
+}
---
+++ PlayerApp/models/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp.models
+{
+    internal class Player
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Age { get; }
+
+       static int DEFAULT_AGE=18;
+
+        public Player(int id, string name) {
+            Id = id;
+            Name = name;
+            Age = DEFAULT_AGE;
+            
+
+
+        }
+
+        public Player (int id, string name, int age):this (id, name) 
+        {
+            Age = age;
+            
+        }
+
+        public static Player WhoIsElder(Player[] players)
+        {
+            Player eldest = players[0];
+            foreach (Player player in players)
+            {
+                if (player.Age > eldest.Age)
+                {
+                    eldest = player;
+                }
+            }
+            return eldest;
+        }
+
+        public override string ToString()
+        {
+            return $"Player Id: {Id}\n" +
+                $"Name: {Name}\n"+
+                $"Age: {Age}";
+        }
+
+
+
+
+
+
+    }
+}
