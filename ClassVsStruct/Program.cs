1cca67a5b6916c040551a3819b3f88372ba88680
17Sampada <91624838+17Sampada@users.noreply.github.com>
2024-07-17 23:41:39 +0530

    Add files via upload

---
+++ ClassVsStruct/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassVsStruct.models;
+
+namespace ClassVsStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Sampada", 22);
+            person.Introduce();
+
+            Point point = new Point(10, 20);
+            point.Display();
+
+            
+            Point anotherPoint = point;
+            anotherPoint.X = 30;
+            anotherPoint.Display(); 
+            point.Display();
+        }
+    }
+}
---
+++ ClassVsStruct/models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVsStruct.models
+{
+    internal class Person
+    {
+        
+        
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public void Introduce()
+            {
+                Console.WriteLine($"Hi, I am {Name} and I am {Age} years old.");
+            }
+        
+    }
+}
