61a75f51bdef144b3bab4f22f094d2d5dc9650a0
17Sampada <91624838+17Sampada@users.noreply.github.com>
2024-07-22 23:51:47 +0530

    Add files via upload

---
+++ FileHandelling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+namespace FileHandelling
+{
+    internal class Program
+    {
+        
+
+            
+            static void Main(string[] args)
+            {
+                string file1 = @"D:\AproSCM'24\FileHandelling\FileHandelling.txt";
+                
+                Console.WriteLine("Reading File using File.ReadAllText()");
+                if (File.Exists(file1))
+                {
+                    string str = File.ReadAllText(file1);
+                    Console.WriteLine(str);
+                }
+
+                File.WriteAllText("D:\\AproSCM'24\\FileHandelling\\FileHandelling.txt", "hello world 2");
+
+            
+
+               
+        }
+        
+    }
+}
---
+++ FileHandellingHtml/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace FileHandellingHtml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Users\sampada.bhadane\Desktop\filehandellinghtml.html";
+
+            Console.WriteLine("Reading File using File.ReadAllLines()");
+            if (File.Exists(file))
+            {
+                string[] lines = File.ReadAllLines(file);
+                foreach (string ln in lines)
+                    Console.WriteLine(ln);
+            }
+
+            string htmlContent = "<!DOCTYPE html>\n" +
+                             "<html>\n" +
+                             "<head>\n" +
+                             "<title>My HTML Page</title>\n" +
+                             "</head>\n" +
+                             "<body>\n" +
+                             "<h1>Hello, Sampada!</h1>\n" +
+                             "</body>\n" +
+                             "</html>";
+
+            string htmlFilePath = "C:\\Users\\sampada.bhadane\\Desktop\\filehandellinghtml.html";
+
+
+            File.WriteAllText(htmlFilePath, htmlContent);
+
+            Console.WriteLine($"HTML content written to {htmlFilePath}");
+            using (StreamWriter sw = new StreamWriter(htmlFilePath, true))
+            {
+                sw.WriteLine("hello first");
+                sw.WriteLine("text written in file");
+            }
+        }
+        
+    }
+}
+
