f1805b0318efb51db6aa2790ff36eae094ecf485
Sampada Vasant Bhadane <Sampada.Bhadane@asl.mum.sst>
2024-09-16 21:14:24 +0530

    sessiontimeout

---
+++ MVC/SessionTimeOutDemoMVC/SessionTimeOutDemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using SessionTimeOutDemoMVC.Models;
+
+namespace SessionTimeOutDemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Session["Student"] = student;
+
+
+                Session.Timeout = 2;
+
+                return RedirectToAction("Details");
+            }
+
+            return View(student);
+        }
+
+
+        public ActionResult Details()
+        {
+
+            if (Session["Student"] == null)
+            {
+                TempData["Message"] = "Session expired! Please enter the details again.";
+                return RedirectToAction("Create");
+            }
+
+
+            Student student = Session["Student"] as Student;
+            return View(student);
+        }
+    }
+}
---
+++ MVC/SessionTimeOutDemoMVC/SessionTimeOutDemoMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SessionTimeOutDemoMVC.MvcApplication" Language="C#" %>
